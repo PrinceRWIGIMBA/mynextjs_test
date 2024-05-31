e4f9dabeb1f23bcf6a3c8b3519f6961bb1d4f7e8
RwigimbaP_dev <princerwigimba@gmail.com>
2024-05-31 16:00:34 +0200

    third commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>hello prince this is your last test before deployment</h1>
+    <h1>Good Job Prince THis is Working well </h1>
   );
 }

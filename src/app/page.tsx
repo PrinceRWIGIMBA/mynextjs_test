5da3ccf740a1f9d4661d3b97182e5137ea0f686b
RwigimbaP_dev <princerwigimba@gmail.com>
2024-06-02 15:13:21 +0200

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>badge   test display </h1>
+   
+    <h1>i have change this in this to test if review is working well and if everything if working good! </h1>
   );
 }
